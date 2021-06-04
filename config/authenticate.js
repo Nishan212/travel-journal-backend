b49c7a8ea3c00a2d32f8d84e3304a6a860847882
Nishan D' Almeida <dalmeidanishan@gmail.com>
2021-06-04 12:39:35 +0530

    name and email returned with token

--- config/authenticate.js
+++ config/authenticate.js
@@ -7,59 +7,59 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const options = {
-	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-	secretOrKey: process.env.SECRET_KEY,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET_KEY,
 };
 
 exports.getToken = function (user) {
-	return jwt.sign(user, process.env.SECRET_KEY, { expiresIn: 360000 });
+    return jwt.sign(user, process.env.SECRET_KEY, { expiresIn: 360000 });
 };
 
 exports.localPassport = passport.use(
-	'local',
-	new LocalStrategy(
-		{
-			usernameField: 'email',
-		},
-		async (email, password, done) => {
-			await User.findOne({ email }, async (err, user) => {
-				if (err) {
-					return done(err);
-				}
+    'local',
+    new LocalStrategy(
+        {
+            usernameField: 'email',
+        },
+        async (email, password, done) => {
+            await User.findOne({ email }, async (err, user) => {
+                if (err) {
+                    return done(err);
+                }
 
-				if (!user) {
-					return done(new Error('Email not registered'));
-				}
+                if (!user) {
+                    return done(new Error('Email not registered'));
+                }
 
-				const isMatch = await bcrypt.compare(password, user.password);
+                const isMatch = await bcrypt.compare(password, user.password);
 
-				if (!isMatch) {
-					return done(new Error('Incorrect password'));
-				}
+                if (!isMatch) {
+                    return done(new Error('Incorrect password'));
+                }
 
-				return done(null, user);
-			});
-		}
-	)
+                return done(null, user);
+            });
+        }
+    )
 );
 
 exports.jwtPassport = passport.use(
-	'jwt',
-	new JwtStrategy(options, async (jwt_payload, done) => {
-		await User.findById(jwt_payload._id, (err, user) => {
-			if (err) {
-				return done(err, false);
-			}
-			if (user) {
-				return done(null, user);
-			} else {
-				return done(null, false);
-			}
-		});
-	})
+    'jwt',
+    new JwtStrategy(options, async (jwt_payload, done) => {
+        await User.findById(jwt_payload._id, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    })
 );
 
 exports.verifyUser = passport.authenticate('jwt', {
-	session: false,
-	failWithError: true,
+    session: false,
+    failWithError: true,
 });
--- routes/users.js
+++ routes/users.js
@@ -6,66 +6,71 @@ const authenticate = require('../config/authenticate');
 const router = express.Router();
 
 router.post('/register', async (req, res, next) => {
-	const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-	try {
-		const existingUser = await User.findOne({
-			email,
-		});
-		if (existingUser)
-			return res.status(400).json({
-				error: 'User with this email exists',
-			});
+    try {
+        const existingUser = await User.findOne({
+            email,
+        });
+        if (existingUser)
+            return res.status(200).json({
+                error: 'User with this email exists',
+            });
 
-		const salt = await bcrypt.genSalt(10);
-		const hashPassword = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
 
-		const user = new User({
-			name: name,
-			email: email,
-			password: hashPassword,
-		});
+        const user = new User({
+            name: name,
+            email: email,
+            password: hashPassword,
+        });
 
-		await user.save();
+        await user.save();
 
-		return res.status(200).json({
-			success: true,
-			message: 'User created',
-		});
-	} catch (err) {
-		console.log(err.message);
-		res.status(404).json({
-			error: err.message,
-		});
-	}
+        return res.status(200).json({
+            success: true,
+            message: 'User created',
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.status(200).json({
+            error: err.message,
+        });
+    }
 });
 
 router.post('/login', async (req, res, next) => {
-	passport.authenticate('local', (err, user, info) => {
-		try {
-			if (err || !user) {
-				return res.status(400).json({
-					error: err !== null ? err.message : 'Body empty',
-				});
-			}
+    passport.authenticate('local', (err, user, info) => {
+        try {
+            if (err || !user) {
+                return res.status(200).json({
+                    error: err !== null ? err.message : 'Body empty',
+                });
+            }
 
-			req.login(user, { session: false }, err => {
-				if (err)
-					return res.status(400).json({
-						error: err.message,
-					});
+            req.login(user, { session: false }, (err) => {
+                if (err)
+                    return res.status(200).json({
+                        error: err.message,
+                    });
 
-				const body = { _id: user._id, email: user.email };
-				const token = authenticate.getToken(body);
+                const body = { _id: user._id, email: user.email };
+                const token = authenticate.getToken(body);
 
-				return res.json({ success: true, token: token });
-			});
-		} catch (err) {
-			return res.status(400).json({
-				err,
-			});
-		}
-	})(req, res, next);
+                return res.json({
+                    success: true,
+                    name: user.name,
+                    email: user.email,
+                    token: token,
+                });
+            });
+        } catch (err) {
+            return res.status(200).json({
+                err,
+            });
+        }
+    })(req, res, next);
 });
 
 module.exports = router;
